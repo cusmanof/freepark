2dce92fdcbd1ccd7002427094c54ff8622878084
cusmanof <cusmanof@gmail.com>
2015-10-08 12:50:44 +0800

    initial

---
+++ freedays.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+
+</body>
+<?php
+phpinfo();
+/*
+$PP = file_get_contents("php://input");
+$data = json_decode($PP);
+
+file_put_contents("post.txt", $PP);
+file_put_contents("p1.txt", count($data->dates));
+file_put_contents("p2.txt", $data->name);
+
+file_put_contents("gson1.txt",json_encode($data));
+*/
+?>
+
+</html>
