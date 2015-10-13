df2324b886b26120909f37bd1db7b18fcbf3ea2c
cusmanof <cusmanof@gmail.com>
2015-10-13 08:54:27 +0800

    working owner

--- createTable.php
+++ createTable.php
@@ -8,49 +8,26 @@ Connect to Cloud SQL
     -Replace "demo-db" with the name of your instance in Cloud SQL
     -Replace "root" with your user name
 */
+//$conn = mysql_connect(":/cloudsql/freepark-1091:frank", "root", "");
 $conn = mysql_connect("localhost", "root", "rootmysql");
 if (!$conn) {
     die('Connect Error (' . mysql__error());
 }
 
-/*
-$conn = mysql_connect(":/cloudsql/freepark-1091:frank", "root", "");
-if (!$conn) {
-    die('Connect Error (' . mysql__error());
-}
-*/
-/*
-//Select Database
-$db_selected = mysql_select_db('freepark', $conn);
-if (!$db_selected) {
-    die ('Can\'t use db : ' . mysql_error());
-}
-//Perform Query
-$result = mysql_query("SELECT name, date  FROM freedays_tbl;");
-//Show Results
-echo "<!doctype html>";
-echo "<body>";
-echo "<h3>Results from Google Cloud SQL</h3>";
-echo "<table class=simpletable border=1>";
-echo "<tr><th align=left>Name</th><th>date</th></tr>";
-while ($row = mysql_fetch_assoc($result)) {
-    echo "<tr><td align=left>" . $row['name'] . "</td>";
-    echo "<td align=center class=addCommas>" . $row['size'] . "</td></tr>";
-}
-echo "</table>";
-*/
 
 //Select Database
 $db_selected = mysql_select_db('test', $conn);
 if (!$db_selected) {
     die ('Can\'t use db : ' . mysql_error());
 }
-$sql = "CREATE TABLE freedays_tbl( ".
-       "owner VARCHAR(40) NOT NULL, ".
-	   "parkId VARCHAR(40) NOT NULL, ".
-       "user VARCHAR(40) , ".
-       "submission_date DATE); ";
-$retval = mysql_query( $sql, $conn );
+$sql = "CREATE TABLE freedays_tbl (
+  owner varchar(40) ,
+  parkId varchar(40),
+  user varchar(40),
+  free_date date,
+  PRIMARY KEY  (parkId,free_date)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+$retval = mysql_query( $sql );
 if(! $retval )
 {
   die('Could not create table: ' . mysql_error());
--- freedays.php
+++ freedays.php
@@ -1,11 +1,8 @@
-<html>
-<body>
-
-
 <?php
-
-$conn = mysql_connect("localhost", "root", "rootmysql");
+/* $conn = mysql_connect(":/cloudsql/freepark-1091:frank", "root", ""); */
+$conn = mysql_connect("localhost:3306", "root", "rootmysql");
 if (!$conn) {
+	file_put_contents("ze.txt",  mysql_error());
     die('Connect Error (' . mysql__error());
 }
 
@@ -13,30 +10,64 @@ if (!$conn) {
 //Select Database
 $db_selected = mysql_select_db('test');
 if (!$db_selected) {
-	file_put_contents("pe2.txt",'Can\'t use db : ' . mysql_error());
+		file_put_contents("ze1.txt",  mysql_error());
     die ('Can\'t use db : ' . mysql_error());
 }
 
 $PP = file_get_contents("php://input");
 $data = json_decode($PP);
-//file_put_contents("pp.txt",$PP);
-
-
-/* $conn = mysql_connect(":/cloudsql/freepark-1091:frank", "root", ""); */
 
 $name = $data->name;
 $parkId = $data->parkId;
-
-for ($i = 0; $i < count($data->dates); $i++) {
-$dd = $data->dates[$i];
-$sql = "INSERT INTO freedays_tbl (name, parkId, date) VALUES ('$name','$parkId','$dd')";
-file_put_contents("pn.txt", $sql);
-$result = mysql_query($sql);
+$cmd = $data->cmd;
+$ret = array();
+$ret[cmd]="ret";
+$ret[name]=$name;
+$ret[parkId]=$parkId;
+$dts = array();
+$ret[dates] = $dts;
+if ($cmd == "get") {
+/*
+	$sql = "CREATE TABLE freedays_tbl (
+	  owner varchar(64) ,
+	  parkId varchar(64),
+	  user varchar(64),
+	  free_date date,
+	  PRIMARY KEY  (parkId,free_date)
+	) ENGINE=InnoDB DEFAULT CHARSET=latin1";
+	
+	$retval = mysql_query( $sql );
+	file_put_contents("ze2.txt",  mysql_error());
+*/
+	
+	$sql = "SELECT free_date from freedays_tbl WHERE parkId='$parkId'";
+	$result = mysql_query($sql);
+	if ($result) {
+		// output data of each row
+		$idx=0;
+		while($row = mysql_fetch_array($result)) {	
+			 $dts[$idx++] = $row[0];
+		}
+		$ret[dates] = $dts;
+	}
+	$en = json_encode($ret);
+	echo $en;
+	file_put_contents("zos.txt", $en );
+} elseif ($cmd == "put") {
+	file_put_contents("zin.txt", $PP);
+	//remove unused entries first.
+	$sql = "DELETE FROM freedays_tbl WHERE parkId='$parkId' AND (user IS NULL OR user='');";
+	$result = mysql_query($sql);
+	for ($i = 0; $i < count($data->dates); $i++) {
+		$dd = $data->dates[$i];
+		$sql = "INSERT INTO freedays_tbl (owner, parkId, free_date) VALUES ('$name','$parkId','$dd');";
+		$result = mysql_query($sql);
+		
+	}
 }
 
 
-//Perform Query
+mysql_close();
+
 
 ?>
-</body>
-</html>
