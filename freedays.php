5bda4e97923a550cc83edf430a268f3e612c893f
cusmanof <cusmanof@gmail.com>
2015-10-14 10:54:25 +0800

    fix 5

--- freedays.php
+++ freedays.php
@@ -2,13 +2,13 @@
 $conn = mysql_connect(":/cloudsql/freepark-1091:frank", "root", "");
 /*$conn = mysql_connect("localhost:3306", "root", "rootmysql"); */
 if (!$conn) {
-	echo 'Connect Error (' . mysql_error());
+	echo 'Connect Error (' . mysql_error();
     die('Connect Error (' . mysql_error());
 }
 
 $db_selected = mysql_select_db('freepark');
 if (!$db_selected) {
-	echo 'Can\'t use db : ' . mysql_error());
+	echo 'Can\'t use db : ' . mysql_error();
     die ('Can\'t use db : ' . mysql_error());
 }	
 
