87ced6cf9f760cafed45776d94a9996102b03a00
frank cusmano <cusmanof@gmail.com>
2015-10-14 03:20:40 +0000

    test

--- freedays.php
+++ freedays.php
@@ -24,68 +24,7 @@ $ret[parkId]=$parkId;
 $ret[dates] = array();
 $ret[used] = array();
 
-//Select Database
 
-/*	
-if ($cmd == "put") {
-	//remove unused entries first.
-	$sql = "DELETE FROM freedays_tbl WHERE parkId='$parkId' AND (userId IS NULL OR userId='')";
-	mysql_query($sql);
-	for ($i = 0; $i < count($data->dates); $i++) {
-		$dd = $data->dates[$i];
-		$sql = "INSERT INTO freedays_tbl (owner, parkId, free_date) VALUES ('$name','$parkId','$dd')";
-		$result = mysql_query($sql);
-		
-	}
-}
-if ( $cmd=="get" || $cmd =="put") {
-	$sql = "SELECT * from freedays_tbl WHERE parkId='$parkId'";
-	$result = mysql_query($sql);
-	if ($result) {
-		// output data of each row
-		$idx=0;
-		while($row = mysql_fetch_array($result)) {	
-			 $dts[$idx] = $row['free_date'];
-			 $used[$idx] = $row['userId'];
-			 $idx++;
-		}
-		$ret[dates] = $dts;
-		$ret[used] = $used;
-	}
-	$en = json_encode($ret);
-	echo $en;
-}
-if ( $cmd=="rel" ) {
-	$sql = "UPDATE freedays_tbl SET userId = NULL WHERE userId ='$data->userId'  AND free_date = '$data->date'";
-	$result = mysql_query($sql);
-}
-if ( $cmd=="res" ) {
-	$sql = "UPDATE freedays_tbl SET userId = '$data->userId' WHERE userId IS NULL  AND free_date = '$data->date' LIMIT 1";
-	$result = mysql_query($sql);
-}
-//if ( $cmd=="all" || $cmd="rel" || $cmd="res") {
-	$sql = "SELECT * from freedays_tbl";
-	$result = mysql_query($sql);
-	if ($result) {
-		// output data of each row
-		$idx=0;
-		while($row = mysql_fetch_array($result)) {
-			 $own[$idx] = $row['owner'];
-			 $prk[$idx] = $row['parkId'];
-			 $usr[$idx] = $row['userId'];
-			 $fre[$idx] = $row['free_date'];
-			 $idx++;
-		}
-		$ret[name] = $own;
-		$ret[parkId] = $prk;
-		$ret[used] = $usr;
-		$ret[dates] = $fre;
-
-	}
-	$en = json_encode($ret);
-	echo $en;
-//}
-*/
 mysql_close();
 
 ?>
